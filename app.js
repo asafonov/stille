4aee225ff2c26fa19f2e1bb67537ca795aeab4b4
asafonov <me@asafonov.org>
2023-04-27 16:54:55 +0200

    login

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-const myUserId = "@asafonov.org:matrix.org"
-const myAccessToken = ""
+const userId = "@asafonov.org:matrix.org"
+const accessToken = ""
+const baseUrl = "https://matrix.org"
 const sdk = require("matrix-js-sdk")
-const matrixClient = sdk.createClient({
-  baseUrl: "https://matrix.org",
-  accessToken: myAccessToken,
-  userId: myUserId,
-})
+const init = async () => {
+  const data = {baseUrl: baseUrl}
 
-matrixClient.startClient(20)
+  if (accessToken) {
+    data['accessToken'] = accessToken
+    data['userId'] = userId
+  }
+
+  const matrixClient = sdk.createClient(data)
+
+  if (! accessToken) {
+    const response = await matrixClient.login('m.login.password', {
+      user: userId,
+      password: ''
+    })
+    console.log(response)
+    console.log(response.access_token)
+  }
+
+  matrixClient.startClient(20)
+}
+
+init()
