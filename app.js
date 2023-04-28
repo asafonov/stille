7561c982568d6a97a5cf446337ad387c00904d3d
asafonov <me@asafonov.org>
2023-04-28 11:41:56 +0200

    login prompt

--- app.js
+++ app.js
@@ -1,27 +1,2 @@
-const userId = "@asafonov.org:matrix.org"
-const accessToken = ""
-const baseUrl = "https://matrix.org"
-const sdk = require("matrix-js-sdk")
-const init = async () => {
-  const data = {baseUrl: baseUrl}
-
-  if (accessToken) {
-    data['accessToken'] = accessToken
-    data['userId'] = userId
-  }
-
-  const matrixClient = sdk.createClient(data)
-
-  if (! accessToken) {
-    const response = await matrixClient.login('m.login.password', {
-      user: userId,
-      password: ''
-    })
-    console.log(response)
-    console.log(response.access_token)
-  }
-
-  matrixClient.startClient(20)
-}
-
-init()
+const login = require('./src/login')
+login.login()
---
+++ src/login/index.js
@@ -0,0 +1,46 @@
+const showLoginForm = f => {
+  const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  readline.question('userId: ', userId => {
+    readline.question('password: ', password => {
+      readline.close()
+      return f(userId, password)
+    })
+  })
+}
+
+const getAccessToken = async (userId, password) => {
+  console.log(userId, password)
+  return
+  const response = await matrixClient.login('m.login.password', {
+    user: userId,
+    password: password
+  })
+  return response.access_token
+}
+
+module.exports.login = async () => {
+  const sdk = require("matrix-js-sdk")
+  const baseUrl = 'https://matrix.org'
+  let accessToken
+  const data = {baseUrl: baseUrl}
+
+  if (accessToken) {
+    data['accessToken'] = accessToken
+    data['userId'] = userId
+  }
+
+  const matrixClient = sdk.createClient(data)
+
+  if (! accessToken) {
+    showLoginForm(async (userId, password) => {
+      const accessToken = await getAccessToken(userId, password)
+      accessToken && matrixClient.startClient()
+    })
+  } else {
+    matrixClient.startClient()
+  }
+
+}
