aa8adff33dd340f58ca2ab4a98c984881d1fd359
asafonov <me@asafonov.org>
2023-07-07 13:37:21 +0200

    add telegram bot option

--- src/bot/telegram/index.js
+++ src/bot/telegram/index.js
@@ -4,6 +4,7 @@ const {NewMessage} = require('telegram/events')
 const input = require('input')
 const cache = require('../../config').init('cache')
 const config = require('../../config').init()
+const antispam = require('../../utils/antispam')
 let client
 
 const getMessage = async message => {
@@ -38,7 +39,36 @@ const initApi = async () => {
   return [apiId, apiHash, botToken]
 }
 
-const init = async callbacks => {
+const subscribe = plugins => {
+  client.addEventHandler(async event => {
+    const message = await getMessage(event.message)
+
+    if (! antispam.isAllowedMessage(message.sender, message.chatId)) {
+      console.log('SPAM', message.sender, message.chatId)
+      const antispamErrorMessage = 'Sorry, the administrator of the bot did not allow me to react to your messages'
+      sendMessage(message.chatId, antispamErrorMessage)
+      return
+    }
+
+    for (let i = 0; i < plugins.length; ++i) {
+      const reply = await plugins[i].onMessage(message.text, message.chatId)
+      reply && sendMessage(message.chatId, reply)
+    }
+  }, new NewMessage({}))
+}
+
+const initPlugins = (plugins, f) => {
+  const fs = []
+
+  for (let i = 0; i < plugins.length; ++i) {
+    const _f = i === 0 ? f : fs[i - 1]
+    fs[i] = 'init' in plugins[i] ? () => plugins[i].init(_f) : _f
+  }
+
+  fs[fs.length - 1]()
+}
+
+const init = async plugins => {
   const [apiId, apiHash, botToken] = await initApi()
   const session = new StringSession(cache.get('session') || '')
   client = new TelegramClient(session, apiId, apiHash, {connectionRetries: 5})
@@ -48,9 +78,10 @@ const init = async callbacks => {
   })
   cache.set('session', client.session.save())
   cache.save()
-  client.addEventHandler(async event => {
-    const message = await getMessage(event.message)
-  }, new NewMessage({}))
+
+  initPlugins(plugins, () => {
+    subscribe(plugins)
+  })
 }
 
 const sendMessage = (to, message) => {
